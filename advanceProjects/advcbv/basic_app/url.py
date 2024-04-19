aa60b5a76efabba7ebe3ffc9d16d4affb6279888
Samadhan Patil <109036065+PatilSama@users.noreply.github.com>
2024-04-19 18:42:42 +0530

    crud operations

--- advanceProjects/advcbv/basic_app/url.py
+++ advanceProjects/advcbv/basic_app/url.py
@@ -4,7 +4,9 @@ from basic_app import views
 app_name = 'basic_app'
 
 urlpatterns = [
-          path('',views.SchoolListView.as_view(),name='list'),
-          path('<pk>', views.SchoolDetailView.as_view(), name='detail'),
-          
+    path("", views.SchoolListView.as_view(), name="list"),
+    path("<int:pk>", views.SchoolDetailView.as_view(), name="detail"),
+    path("create/", views.SchoolCreateView.as_view(), name="create"),
+    path("update/<int:pk>/", views.SchoolUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", views.SchooleDeleteView.as_view(), name="delete"),
 ]
